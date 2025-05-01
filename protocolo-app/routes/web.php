8cc4db88befb790a49e08ae0006cf96927c35a91
Filip3ra <filipijardimrm@gmail.com>
2025-05-01 19:15:00 -0300

    feat: login de usuário adicionado

--- protocolo-app/routes/web.php
+++ protocolo-app/routes/web.php
@@ -1,9 +1,43 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProtocoloController;
 
-Route::get('/protocolo/novo', [ProtocoloController::class, 'create'])->name('protocolo.create');
-Route::post('protocolo', [ProtocoloController::class, 'store'])->name('protocolo.store');
+/**
+ * Rotas Públicas
+ */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Rota para exibir formulário
+Route::get('/protocolo/criar', [ProtocoloController::class, 'create'])->name('protocolo.create');
+Route::post('/protocolo', [ProtocoloController::class, 'store'])->name('protocolo.store');
+
+// Rota de consulta
 Route::get('/consulta', [ProtocoloController::class, 'formConsulta'])->name('consulta.form');
 Route::post('/consulta', [ProtocoloController::class, 'consulta'])->name('consulta.resultado');
+//Route::get('/consulta/{numero}', [ProtocoloController::class, 'resultado'])->name('consulta.resultado');
+
+
+/**
+ * Rotas Protegidas
+ */
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function (){
+    Route::get('dashboard', [ProtocoloController::class, 'dashboard'])->name('dashboard');
+});
+
+require __DIR__.'/auth.php';
